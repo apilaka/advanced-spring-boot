af71891a02d7d050b669702123840a86852ea60e
apilaka <apilaka@yahoo.ca>
2022-10-24 09:26:24 -0400

    first commit

---
+++ src/main/java/com/pilaka/advancedspringboot/AdvancedSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pilaka.advancedspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pilaka/advancedspringboot/AdvancedSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pilaka.advancedspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
